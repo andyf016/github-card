00e9aafe3bceebe3775c55c4cb457310f265f224
Andrew Fillenwarth <andyf016@gmail.com>
2020-02-07 10:06:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Button } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
@@ -8,21 +8,39 @@ class App extends Component {
     active: false
   }
   handleToggle = (event) => {
-    console.log("Click")
-    fetch("https://api.github.com/users/davegregg")
-    .then(response => response.json())
-    .then(responseBody => {
-      console.log(responseBody)
-      this.setState({user: responseBody})
+    if (this.state.active === false){
+      fetch("https://api.github.com/users/davegregg")
+      .then(response => response.json())
+      .then(responseBody => {
+       console.log(responseBody)
+        this.setState({user: responseBody, active: true})
 
     })
+    } else {
+      this.setState({active: false})
+    }
+  
+    
 
       
   }
 
   render(){
-      return <button onClick = {this.handleToggle}>Toggle User</button>
-  }
+      return (
+      <>
+      <Button onClick = {this.handleToggle}>Toggle User</Button>
+      {this.state.active && (
+      <>
+      <img src = {this.state.user.avatar_url} alt="user profile"/>
+        <h1>{this.state.user.name}</h1>
+        <p>Bio: {this.state.user.bio}</p>
+        <p>Followers: {this.state.user.followers}</p>
+        </>
+        )}
+      
+      </>
+      )
+    }
 
 }
 
